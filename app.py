e5766147e549245a1e80f0a5f83b0af72bcb562e
zeinbahsein <sayyidhusain14@gmail.com>
2024-10-23 14:23:31 +0700

    penambahan judul

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
+# Judul Aplikasi
+st.title("Analisa Success Rate Booking period Maret - Oktober")
+
 # Upload file CSV dari komputer
 uploaded_file = st.file_uploader("Pilih file CSV", type="csv")
 
